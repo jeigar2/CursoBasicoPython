b46075b28c1eba79b6dde790c49468f1b1bbf89b
jeigar2 <jeigar2@gmail.com>
2021-10-15 01:56:07 +0200

    conversor $2€ y €2$
    Programa conversor.py de dolares a euros, (ratio conversión  0.8623)
    programa conversor_2.py de euros a dolares (ratio conversión 1.1597)

---
+++ conversor.py
@@ -0,0 +1,5 @@
+dolares = input("¿Cuántos dólares tienes? ")
+dolares = float(dolares)
+valor_euros = 0.8623
+euros = dolares * valor_euros
+print ("Tienes " + str(round(euros, 2)) + "€")
---
+++ conversor_2.py
@@ -0,0 +1,5 @@
+euros = input("¿Cuántos euros tienes? ")
+euros = float(euros)
+valor_dolares = 1.1597
+dolares = euros * valor_dolares
+print ("Tienes $" + str(round(dolares, 2)))
