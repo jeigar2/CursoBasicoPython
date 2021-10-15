227103ecb961661651447bf14349630ba1b5576f
Jesus García <jesus.garcia.traffickers@gmail.com>
2021-10-15 02:58:17 +0200

    refactorizado usando funciones

--- conversorMultiple.py
+++ conversorMultiple.py
@@ -1,3 +1,9 @@
+def conversion(nombre_moneda, conversor, prefijo, sufijo):
+    moneda = input('¿Cuántos ' + nombre_moneda + ' tienes? ')
+    moneda = float(moneda)
+    cambio = moneda * conversor
+    print('Tienes '+ prefijo + str(round(cambio, 2)) + sufijo)
+
 menu = """
 Bienvenido al conversor de monedas
 
@@ -11,26 +17,10 @@ Elige una opción (1,2,3):
 opcion = int(input(menu))
 
 if opcion == 1: #Euros a Dolares
-    moneda = input('¿Cuántos Euros tienes? ')
-    moneda = float(moneda)
-    conversor = 1.1597
-    prefijo = "$"
-    sufijo = ""
+    conversion('Euros', 1.1597,'$','')
 elif opcion == 2: #Dolares a Euros
-    moneda = input('¿Cuántos Dolares tienes? ')
-    moneda = float(moneda)
-    conversor = 0.8623
-    prefijo = ""
-    sufijo = "€"
+    conversion('Dolares', 0.8623,'','€')
 elif opcion == 3: #Bitcoin a Euros
-    moneda = input('¿Cuántos Bitcoin tienes? ')
-    moneda = float(moneda)
-    conversor = 49387.466
-    prefijo = ""
-    sufijo = "€"
+    conversion('Bitcoin', 49387.466,'','€')
 else: 
     print('Escribe una opción correcta, por favor (1,2,3) ')
-    quit()
-
-cambio = moneda * conversor
-print('Tienes '+ prefijo + str(round(cambio, 2)) + sufijo)
