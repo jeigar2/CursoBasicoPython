75f2e71201cda9d0cd7f20eb202cf26e5a660fbc
Jesus García <jesus.garcia.traffickers@gmail.com>
2021-10-15 03:52:47 +0200

    agregando bucles, potencia de 2 hasta la primera potencia con 1000

---
+++ potencia.py
@@ -0,0 +1,11 @@
+def f_potencia(potencia):
+    print("2 elevado a " + str(potencia) + " es igual a " + str(2**potencia))
+    return 2**potencia
+
+def run():
+    i = 0
+    while f_potencia(i) < 1000:
+        i += 1
+
+if __name__ == "__main__":
+    run()
