30ccb917e32f6a4ded04cf3b88dce40f384d249a
Jesus García <jesus.garcia.traffickers@gmail.com>
2021-10-16 01:41:42 +0200

    variante del juego fizzBuzz, no imprimir multiplos de 7 y salir con 42

---
+++ fizz_buzz_fizzBuzz.py
@@ -0,0 +1,30 @@
+def es_divisible(numero, divisor):
+    return numero % divisor == 0
+
+def es_divisible2(numero, divisor1, divisor2):
+    return numero % divisor1 == 0  and numero % divisor2 == 0
+
+def filtrarNumero(numero):
+    if es_divisible2(numero, 3, 5):
+        return "'fizzBuzz'"
+    elif es_divisible(numero, 3):
+        return "'fizz'"
+    elif es_divisible(numero, 5):
+        return "'buzz'"
+    else:
+        return str(numero)
+
+def run():
+    contador = 0
+    LIMITE = int(input("introduce el número límite del juego: "))
+    while (contador < LIMITE):
+        contador += 1
+        if contador == 42:
+            print ("cuarenta y dos")
+            break
+        elif contador % 7 == 0:
+            continue
+        print(filtrarNumero(contador))
+
+if __name__ == "__main__":
+    run()
