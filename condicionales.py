d42dc4c07b1a556553e4612ffa5c93fd7d81428c
Jesus García <jesus.garcia.traffickers@gmail.com>
2021-10-15 02:10:26 +0200

    programa con if pide un numero y comprueba si igual, mayor o menor a 5

---
+++ condicionales.py
@@ -0,0 +1,8 @@
+numero = int(input("escribe un número:"))
+
+if numero > 5:
+    print("Has escrito un número mayor a 5")
+elif numero < 5:
+    print("Has escrito un número menor a 5")
+else:
+    print("has escrito el número 5")
